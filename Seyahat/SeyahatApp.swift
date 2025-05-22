037158e0501971e8b5fadeb06de4b684c66be245
gorkemgurel <35767747+gorkemgurel@users.noreply.github.com>
2025-05-22 20:31:29 +0300

    Initial commit

---
+++ Seyahat/SeyahatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeyahatApp.swift
+//  Seyahat
+//
+//  Created by Gorkem Gurel on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SeyahatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
